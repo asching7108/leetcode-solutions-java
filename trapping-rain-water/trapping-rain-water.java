8baf1d200397c66a2ee0218689ff5025bfd1bd80
asching7108 <53709035+asching7108@users.noreply.github.com>
2022-06-10 15:52:27 -0400

    Time: 14 ms, Memory: 49.9 MB - LeetHub

---
+++ trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        Stack<Integer> stack = new Stack();
+        int total = 0, prev, top, dist;
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                prev = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                top = stack.peek();
+                dist = i - top - 1;
+                total += dist * Math.min(height[i] - height[prev], height[top] - height[prev]);
+            }
+            stack.push(i);
+        }
+        return total;
+    }
+}
